f780c37832d555deb896a1d6aadc3fd439b85eef
Aristos2504 <163442096+Aristos2504@users.noreply.github.com>
2024-04-17 10:04:27 +0300

    Update OrderGenerator.java

--- OrderGenerator.java
+++ OrderGenerator.java
@@ -1,10 +1,32 @@
 package hw4;
-
 public class OrderGenerator {
+	int[] num;
+	int[] tOrder;
+	int[] tReq;
+	int[] nPp;
+	int[] nPc;
+	int[] nPs;
+	int[] nPm;
+	int[] nPf;
+
+	public OrderGenerator(int numOfOrders) {//int[] num, int[] tOrder, int[] tReq, int[] nPp, int[] nPc, int[] nPs, int[] nPm, int[] nPf) {
+		this.num = new int[numOfOrders];//(length?)
+		
+		this.tOrder = tOrder;
+		this.tReq = tReq;
+		this.nPp = nPp;
+		this.nPc = nPc;
+		this.nPs = nPs;
+		this.nPm = nPm;
+		this.nPf = nPf;
+
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int numOfOrders = Integer.parseInt(args[0]);
+		OrderGenerator orders= new OrderGenerator(numOfOrders);
 	}
 
 }
+
