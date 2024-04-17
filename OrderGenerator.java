c9176365cae6eaeb93ad7fe516fed306f2ee2c62
Aristos2504 <163442096+Aristos2504@users.noreply.github.com>
2024-04-17 16:35:53 +0300

    Update OrderGenerator.java

--- OrderGenerator.java
+++ OrderGenerator.java
@@ -1,26 +1,29 @@
 package hw4;
+
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Random;
-public class OrderGenerator {
-	int[] num;
-	int[] tOrder;
-	int[] tReq;
-	int[] nPp;
-	int[] nPc;
-	int[] nPs;
-	int[] nPm;
-	int[] nPf;
 
-	public OrderGenerator(int numOfOrders) {
-		this.num = new int[numOfOrders];
-		this.tOrder = fillTimeOrder(numOfOrders);
-      		 this.tReq = fillDesiredDeliveryTimes(numOfOrders);
-		this.nPp = new int[numOfOrders];
-		this.nPc = new int[numOfOrders];
-		this.nPs = new int[numOfOrders];
-		this.nPm = new int[numOfOrders];
-		this.nPf = new int[numOfOrders];
+public class OrderGenerator {
+    int[] num;
+    int[] tOrder;
+    int[] tReq;
+    int[] nPp;
+    int[] nPc;
+    int[] nPs;
+    int[] nPm;
+    int[] nPf;
+
+    public OrderGenerator(int numOfOrders) {
+        this.num = new int[numOfOrders];
+        this.nPp = new int[numOfOrders];
+        this.nPc = new int[numOfOrders];
+        this.nPs = new int[numOfOrders];
+        this.nPm = new int[numOfOrders];
+        this.nPf = new int[numOfOrders];
+        this.tOrder = fillTimeOrder(numOfOrders);
+        this.tReq = fillDesiredDeliveryTimes(numOfOrders, this.nPf);
+    }
 
     public static int[] fillTimeOrder(int numOfOrders) {
         Random rand = new Random();
@@ -32,7 +35,7 @@ public class OrderGenerator {
         return tOrder;
     }
 
-    public static int[] fillDesiredDeliveryTimes(int numOfOrders) {
+    public static int[] fillDesiredDeliveryTimes(int numOfOrders, int[] nPf) {
         Random rand = new Random();
         int[] tReq = new int[numOfOrders];
         for (int i = 0; i < tReq.length; i++) {
@@ -47,41 +50,57 @@ public class OrderGenerator {
         return tReq;
     }
 
-	public void sortOrders(int numOfOrders) {
-		int tempO, tempN, tempR, tempPp, tempPc, tempPs, tempPm, tempPf;
-		for (int i = 0; i < numOfOrders-1; i++) {
-			if(tOrder[i]>tOrder[i+1]) {
-				tempO=tOrder[i]; tempN=num[i]; tempR=tReq[i];tempPp=nPp[i]; tempPc=nPc[i];tempPs=nPs[i]; tempPm=nPm[i]; tempPf=nPf[i];
-				tOrder[i]=tOrder[i+1]; num[i]=num[i+1]; tReq[i]=tReq[i+1]; nPp[i]=nPp[i+1]; nPc[i]=nPc[i+1]; nPs[i]=nPs[i+1]; nPm[i]=nPm[i+1];nPf[i]=nPf[i+1];
-				tOrder[i+1]=tempO; num[i+1]=tempN; tReq[i+1]=tempR; nPp[i+1]=tempPp; nPc[i+1]=tempPc; nPs[i+1]=tempPs; nPm[i+1]=tempPm; nPf[i+1]=tempPf;
-			}
-		}
-		
-	}
-	 static void writeOrders(int[] num, int[] tOrder, int[] tReq, int[] nPp, int[] nPc, int[] nPs, int[] nPm, int[] nPf) {
-	        try {
-	            PrintWriter writer = new PrintWriter("orders.txt");
-	            writer.println(num.length); 
-	            for (int i = 0; i < num.length; i++) {
-	                writer.println(num[i] + " " + tOrder[i] + " " + tReq[i] + " "
-	                        + nPp[i] + " " + nPc[i] + " " + nPs[i] + " " + nPm[i] + " " + nPf[i]);
-	            }
-	            writer.close();
-	        } catch (FileNotFoundException e) {
-	            System.out.println("An error occurred while writing the orders file.");
-	            e.printStackTrace();
-	        }
-	    }		
+    public void sortOrders() {
+        int tempO, tempN, tempR, tempPp, tempPc, tempPs, tempPm, tempPf;
+        for (int i = 0; i < num.length - 1; i++) {
+            if (tOrder[i] > tOrder[i + 1]) {
+                tempO = tOrder[i];
+                tempN = num[i];
+                tempR = tReq[i];
+                tempPp = nPp[i];
+                tempPc = nPc[i];
+                tempPs = nPs[i];
+                tempPm = nPm[i];
+                tempPf = nPf[i];
+                tOrder[i] = tOrder[i + 1];
+                num[i] = num[i + 1];
+                tReq[i] = tReq[i + 1];
+                nPp[i] = nPp[i + 1];
+                nPc[i] = nPc[i + 1];
+                nPs[i] = nPs[i + 1];
+                nPm[i] = nPm[i + 1];
+                nPf[i] = nPf[i + 1];
+                tOrder[i + 1] = tempO;
+                num[i + 1] = tempN;
+                tReq[i + 1] = tempR;
+                nPp[i + 1] = tempPp;
+                nPc[i + 1] = tempPc;
+                nPs[i + 1] = tempPs;
+                nPm[i + 1] = tempPm;
+                nPf[i + 1] = tempPf;
+            }
+        }
+    }
 
-	public static void main(String[] args) {
- 	   int numOfOrders = Integer.parseInt(args[0]);
-   	 OrderGenerator orders = new OrderGenerator(numOfOrders);
-  	  fillTimeOrder(orders.tOrder);
-    	fillDesiredDeliveryTimes(orders.tReq, orders.nPf);
-   	 orders.sortOrders(numOfOrders);
-   	 writeOrders(orders.num, orders.tOrder, orders.tReq, orders.nPp, orders.nPc, orders.nPs, orders.nPm, orders.nPf);
-		}
+    public static void writeOrders(int[] num, int[] tOrder, int[] tReq, int[] nPp, int[] nPc, int[] nPs, int[] nPm, int[] nPf) {
+        try {
+            PrintWriter writer = new PrintWriter("orders.txt");
+            writer.println(num.length);
+            for (int i = 0; i < num.length; i++) {
+                writer.println(num[i] + " " + tOrder[i] + " " + tReq[i] + " "
+                        + nPp[i] + " " + nPc[i] + " " + nPs[i] + " " + nPm[i] + " " + nPf[i]);
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred while writing the orders file.");
+            e.printStackTrace();
+        }
+    }
 
-
+    public static void main(String[] args) {
+        int numOfOrders = Integer.parseInt(args[0]);
+        OrderGenerator orders = new OrderGenerator(numOfOrders);
+        orders.sortOrders();
+        writeOrders(orders.num, orders.tOrder, orders.tReq, orders.nPp, orders.nPc, orders.nPs, orders.nPm, orders.nPf);
+    }
 }
-
