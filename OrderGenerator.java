71b28b37a961468ef41d8737dab66b90b35c4055
AndreasCS4 <158460202+AndreasCS4@users.noreply.github.com>
2024-04-18 17:37:03 +0300

    Update OrderGenerator.java

--- OrderGenerator.java
+++ OrderGenerator.java
@@ -5,73 +5,252 @@ import java.io.PrintWriter;
 import java.util.Random;
 
 public class OrderGenerator {
-    int[] num;
-    int[] tOrder;
-    int[] tReq;
-    int[] nPp;
-    int[] nPc;
-    int[] nPs;
-    int[] nPm;
-    int[] nPf;
+	int[] num;
+	int[] tOrder;
+	int[] tReq;
+	int[] nPp;
+	int[] nPc;
+	int[] nPs;
+	int[] nPm;
+	int[] nPf;
+	int[] pittaCounter;
 
-    public OrderGenerator(int numOfOrders) {
-        this.num = new int[numOfOrders];
-        this.nPp = new int[numOfOrders];
-        this.nPc = new int[numOfOrders];
-        this.nPs = new int[numOfOrders];
-        this.nPm = new int[numOfOrders];
-        this.nPf = new int[numOfOrders];
-        this.tOrder = fillTimeOrder(numOfOrders);
-        this.tReq = fillDesiredDeliveryTimes(numOfOrders, this.nPf);
-    }
-
-    public static int[] fillTimeOrder(int numOfOrders) {
-    Random rand = new Random();
-    int[] tOrder = new int[numOfOrders];
-    // Define the opening and closing times in minutes from 18:00 to 23:00
-    int openingTime = 0; // 18:00
-    int closingTime = 300; // 23:00
-    // Define the mean and standard deviation for Gaussian distribution
-    double mean = 180; // Mean time in minutes (21:00)
-    double stdDev = 60; // Standard deviation
-
-    for (int i = 0; i < tOrder.length; i++) {
-        // Generate a random value following Gaussian distribution
-        double gaussianTime = stdDev * rand.nextGaussian() + mean;
-        // Ensure the generated time is within the opening and closing hours
-        int orderTime = (int) Math.round(Math.max(openingTime, Math.min(closingTime, gaussianTime)));
-        tOrder[i] = orderTime;
-    }
-    return tOrder;
-}
-
-    public static int[] fillDesiredDeliveryTimes(int numOfOrders, int[] nPf) {
-    Random rand = new Random();
-    int[] tReq = new int[numOfOrders];
-    // Define the opening and closing times in minutes from 18:00 to 24:00
-    int openingTime = 0; // 18:00
-    int closingTime = 360; // 24:00
-    // Define the mean and standard deviation for Gaussian distribution
-    double mean = 180; // Mean time in minutes (21:00)
-    double stdDev = 60; // Standard deviation
-
-    for (int i = 0; i < tReq.length; i++) {
-        // Generate a random value following Gaussian distribution
-        double gaussianTime = stdDev * rand.nextGaussian() + mean;
-        // Ensure the generated time is within the opening and closing hours
-        int deliveryTime = (int) Math.round(Math.max(openingTime, Math.min(closingTime, gaussianTime)));
-        // Adjust delivery time based on the number of pittas
-        if (nPf[i] > 10) {
-            // If the number of pittas is more than 10, delivery time can't be less than 60 minutes
-            deliveryTime += 30 + rand.nextInt(150); // Random additional time between 30 minutes to 3 hours
-        } else {
-            deliveryTime += 30 + rand.nextInt(180); // Random additional time between 30 minutes to 3 hours
+	public OrderGenerator(int numOfOrders) {
+		this.num = new int[numOfOrders];
+		this.tOrder = new int[numOfOrders];
+		this.tReq = new int[numOfOrders];
+		this.nPp = new int[numOfOrders];
+		this.nPc = new int[numOfOrders];
+		this.nPs = new int[numOfOrders];
+		this.nPm = new int[numOfOrders];
+		this.nPf = new int[numOfOrders];
+		this.pittaCounter= new int[numOfOrders];
+	}
+	
+	public static int generateRandomNumber(int x, int y) {
+        // Check if x is greater than y
+        if (x > y) {
+            // Swap values of x and y
+            int temp = x;
+            x = y;
+            y = temp;
         }
-        tReq[i] = deliveryTime;
+        
+        // Create a Random object
+        Random rand = new Random();
+        
+        // Generate a random number between x and y (both inclusive)
+        int randomNumber = rand.nextInt((y - x) + 1) + x;
+        
+        return randomNumber;
     }
-    return tReq;
-}
+	
+	public   void addPitta(int x, int y, int num) {
+		int random;
+		
+		for(int i=x;i<y;i++) {
+			for(int j=0;j<num;j++) {
+			random=generateRandomNumber(1,4);
+			if(random==1) {
+				this.nPp[i]++;
+			}
+			else if(random==2) {
+				this.nPc[i]++;
+			}
+			else if(random==3) {
+				this.nPs[i]++;
+			}
+			else if(random==4) {
+				this.nPm[i]++;
+			}
+			this.pittaCounter[i]++;
+			}
+		}
+		
+	}
+	
+	public   void addMorePitta(int x, int y) {
+		int random;
+		int num;
+		for(int i=x;i<y;i++) {
+			num=generateRandomNumber(5,20);
+			for(int j=0;j<num;j++) {
+			random=generateRandomNumber(1,4);
+			if(random==1) {
+				this.nPp[i]++;
+			}
+			else if(random==2) {
+				this.nPc[i]++;
+			}
+			else if(random==3) {
+				this.nPs[i]++;
+			}
+			else if(random==4) {
+				this.nPm[i]++;
+			}
+			this.pittaCounter[i]++;
+			}
+		}
+	}
+	
+	public void addFries() {
+	    int totalPittas = 0;
+	    for (int count : pittaCounter)
+	        totalPittas += count;
 
+	    int friesForOnePitta = (int) (totalPittas * 0.6);
+	    int friesForNoPitta = (int) (totalPittas * 0.35);
+
+//	    System.out.println("Total Pittas: " + totalPittas);
+//	    System.out.println("Fries for 60% of Pittas: " + friesForOnePitta);
+//	    System.out.println("Fries for 35% of Pittas: " + friesForNoPitta);
+
+	    int pittasCovered = 0;
+	    for (int i = 0; i < pittaCounter.length; i++) {
+	        pittasCovered += pittaCounter[i];
+	        if (pittasCovered <= friesForOnePitta) {
+	            nPf[i] = pittaCounter[i];
+	        } else if (pittasCovered <= friesForOnePitta + friesForNoPitta && pittasCovered >= friesForOnePitta) {
+	            nPf[i] = 0;
+	        } else {
+	            nPf[i] = pittaCounter[i] * 2;
+	        }
+	    }
+	}
+
+
+
+
+	
+	public void generateOrders(int numOfOrders) {
+		int onePitta= (int)(numOfOrders *0.2);
+		int twoPitta= (int)(numOfOrders *0.35 + onePitta);
+		int threePitta=(int)(numOfOrders *0.1 + twoPitta);
+		int fourPitta= (int)(numOfOrders *0.2 + threePitta);
+		int fiveOrMorePitta= (int)(numOfOrders *0.15 + fourPitta);
+		
+		
+	for(int i=0;i<numOfOrders;i++) {
+			this.num[i]=i+1;
+//			System.out.print(this.num[i]+ "\t");
+			
+		}
+//	System.out.println();
+
+//		System.out.print(generateRandomNumber(1,4));
+//		
+//		System.out.println();
+		
+	
+	
+	
+		addPitta(0,onePitta,1);
+	
+	
+		addPitta(onePitta,twoPitta,2);
+	
+	
+		addPitta(twoPitta,threePitta,3);
+	
+	
+		addPitta(threePitta,fourPitta,4);
+		
+		addMorePitta(fourPitta,fiveOrMorePitta);
+	
+		addFries();
+	
+		this.tOrder=fillTimeOrder(numOfOrders);
+		this.tReq=fillDesiredDeliveryTimes(numOfOrders, this.pittaCounter, this.tOrder);
+		
+		sortOrders();
+		
+		writeOrders(this.num, this.tOrder, this.tReq, this.nPp, this.nPc, this.nPs, this.nPm, this.nPf);
+	
+//		for(int i=0;i<numOfOrders;i++) {
+//			System.out.print(nPp[i]+"\t");
+//			
+//		}
+//		
+//		System.out.println();
+//		
+//		for(int i=0;i<numOfOrders;i++) {
+//			System.out.print(nPc[i]+"\t");
+//			
+//		}
+//		
+//		System.out.println();
+//		
+//		for(int i=0;i<numOfOrders;i++) {
+//			System.out.print(nPs[i]+"\t");
+//			
+//		}
+//		
+//		System.out.println();
+//		
+//		for(int i=0;i<numOfOrders;i++) {
+//			System.out.print(nPm[i]+"\t");
+//			
+//		}
+//		System.out.println();
+//		
+////		for(int i=0;i<numOfOrders;i++) {
+////			System.out.print(pittaCounter[i]+"\t");
+////			
+////		}
+////		System.out.println();
+//		
+//		for(int i=0;i<numOfOrders;i++) {
+//			System.out.print(nPf[i]+"\t");
+//			
+//		}
+//		System.out.println();
+	}
+	
+	public static int[] fillTimeOrder(int numOfOrders) {
+	    Random rand = new Random();
+	    int[] tOrder = new int[numOfOrders];
+	    // Define the opening and closing times in minutes from 18:00 to 23:00
+	    int openingTime = 0; // 18:00
+	    int closingTime = 300; // 23:00
+	    // Define the mean and standard deviation for Gaussian distribution
+	    double mean = 180; // Mean time in minutes (21:00)
+	    double stdDev = 60; // Standard deviation
+
+	    for (int i = 0; i < tOrder.length; i++) {
+	        // Generate a random value following Gaussian distribution
+	        double gaussianTime = stdDev * rand.nextGaussian() + mean;
+	        // Ensure the generated time is within the opening and closing hours
+	        int orderTime = (int) Math.round(Math.max(openingTime, Math.min(closingTime, gaussianTime)));
+	        tOrder[i] = orderTime;
+	    }
+	    return tOrder;
+	}
+	public static int[] fillDesiredDeliveryTimes(int numOfOrders, int[] pittaCounter, int[] tOrder) {
+	    Random rand = new Random();
+	    int[] tReq = new int[numOfOrders];
+	    // Define the opening and closing times in minutes from 18:00 to 24:00
+	    int openingTime = 0; // 18:00
+	    int closingTime = 360; // 24:00
+	    // Define the mean and standard deviation for Gaussian distribution
+	    double mean = 180; // Mean time in minutes (21:00)
+	    double stdDev = 60; // Standard deviation
+
+	    for (int i = 0; i < tReq.length; i++) {
+	        // Generate a random value following Gaussian distribution
+	        double gaussianTime = stdDev * rand.nextGaussian() + mean;
+	        // Ensure the generated time is within the opening and closing hours
+	        int deliveryTime = (int) Math.round(Math.max(openingTime, Math.min(closingTime, gaussianTime)));
+	        // Adjust delivery time based on the number of pittas
+	        if (pittaCounter[i] > 10) {
+	            // If the number of pittas is more than 10, delivery time can't be less than 60 minutes
+	            deliveryTime += 30 + rand.nextInt(150) +tOrder[i]; // Random additional time between 30 minutes to 3 hours
+	        } else {
+	            deliveryTime += 30 + rand.nextInt(180)+ tOrder[i]; // Random additional time between 30 minutes to 3 hours
+	        }
+	        tReq[i] = deliveryTime;
+	    }
+	    return tReq;
+	}
 
    public void sortOrders() {
     for (int i = 0; i < num.length - 1; i++) {
@@ -112,9 +291,11 @@ public class OrderGenerator {
         try {
             PrintWriter writer = new PrintWriter("orders.txt");
             writer.println(num.length);
+            writer.println("Num:" + "\t" + "tOrder:" + "\t" + "tReq:" + "\t"
+                    + "nPp:" + "\t" + "nPc:" + "\t" + "nPs:" + "\t" + "nPm:" + "\t" + "nPf:");
             for (int i = 0; i < num.length; i++) {
-                writer.println(num[i] + " " + tOrder[i] + " " + tReq[i] + " "
-                        + nPp[i] + " " + nPc[i] + " " + nPs[i] + " " + nPm[i] + " " + nPf[i]);
+                writer.println(num[i] + "\t" + tOrder[i] + "\t" + tReq[i] + "\t"
+                        + nPp[i] + "\t" + nPc[i] + "\t" + nPs[i] + "\t" + nPm[i] + "\t" + nPf[i]);
             }
             writer.close();
         } catch (FileNotFoundException e) {
@@ -122,11 +303,13 @@ public class OrderGenerator {
             e.printStackTrace();
         }
     }
+	
+	public static void main(String[] args) {
+		int numOfOrders = Integer.parseInt(args[0]);
+		OrderGenerator orders= new OrderGenerator(numOfOrders);
+		orders.generateOrders(numOfOrders);
+		
+		
+	}
 
-    public static void main(String[] args) {
-        int numOfOrders = Integer.parseInt(args[0]);
-        OrderGenerator orders = new OrderGenerator(numOfOrders);
-        orders.sortOrders();
-        writeOrders(orders.num, orders.tOrder, orders.tReq, orders.nPp, orders.nPc, orders.nPs, orders.nPm, orders.nPf);
-    }
 }
